aa9bac71002c999dac948995526ca9b99d74368b
makeman0405 <71001872+makeman0405@users.noreply.github.com>
2020-10-08 13:51:52 +0900

    MediaService 개선
    에러 처리 로직 수정

--- YouTv/app/src/main/java/com/goldenmelon/youtv/service/MediaService.kt
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/service/MediaService.kt
@@ -202,12 +202,21 @@ class MediaService : Service() {
                     override fun onPlayerError(error: ExoPlaybackException) {
                         super.onPlayerError(error)
                         Toast.makeText(
-                            applicationContext,
-                            "Playback Error: An error has occurred(${error.type})",
-                            Toast.LENGTH_LONG
-                        ).show()
 
-                        //sendBroadcast(Intent(ACTION_QUIT))
+                        if (isNetworkAvailable(applicationContext)) {
+                            Toast.makeText(
+                                applicationContext,
+                                "Playback Error: An error has occurred(${error.type})",
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            sendBroadcast(Intent(ACTION_QUIT))
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                R.string.popup_msg_please_check_the_network, Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 })
                 setAudioAttributes(
