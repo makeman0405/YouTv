b56e21146a380f7cde2dc98ed59c597a66cc0c21
makeman0405 <71001872+makeman0405@users.noreply.github.com>
2020-10-08 13:54:18 +0900

    Revert "MediaService 개선"
    This reverts commit aa9bac71002c999dac948995526ca9b99d74368b.

--- YouTv/app/src/main/java/com/goldenmelon/youtv/service/MediaService.kt
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/service/MediaService.kt
@@ -202,21 +202,12 @@ class MediaService : Service() {
                     override fun onPlayerError(error: ExoPlaybackException) {
                         super.onPlayerError(error)
                         Toast.makeText(
+                            applicationContext,
+                            "Playback Error: An error has occurred(${error.type})",
+                            Toast.LENGTH_LONG
+                        ).show()
 
-                        if (isNetworkAvailable(applicationContext)) {
-                            Toast.makeText(
-                                applicationContext,
-                                "Playback Error: An error has occurred(${error.type})",
-                                Toast.LENGTH_LONG
-                            ).show()
-
-                            sendBroadcast(Intent(ACTION_QUIT))
-                        } else {
-                            Toast.makeText(
-                                applicationContext,
-                                R.string.popup_msg_please_check_the_network, Toast.LENGTH_SHORT
-                            ).show()
-                        }
+                        //sendBroadcast(Intent(ACTION_QUIT))
                     }
                 })
                 setAudioAttributes(
