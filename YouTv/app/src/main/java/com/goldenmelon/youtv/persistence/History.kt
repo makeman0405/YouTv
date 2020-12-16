f24a06ccfd34755c751a0f4c8c919917cc7d8598
makeman0405 <71001872+makeman0405@users.noreply.github.com>
2020-12-16 20:18:56 +0900

    Create History.kt
    Room Entitiy Class 생성

---
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/persistence/History.kt
@@ -0,0 +1,35 @@
+package com.goldenmelon.youtv.persistence
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+import com.goldenmelon.youtv.datas.Content
+import java.util.* //Date
+
+@Entity
+data class History private constructor(
+    @PrimaryKey
+    val videoId: String,
+
+    @ColumnInfo(name = "thumbnail") val thumbnail: String?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "sub_title") val subTitle: String? = null,
+    @ColumnInfo(name = "length_text") val lengthText: String? = null,
+    @ColumnInfo(name = "owner_text") val ownerText: String? = null,
+    @ColumnInfo(name = "date") val date: Date? = null
+) {
+    companion object {
+        fun create(content: Content, date: Date): History {
+            return History(
+                content.videoId,
+                content.thumbnail,
+                content.title,
+                content.subTitle,
+                content.lengthText,
+                content.ownerText,
+                date
+            )
+        }
+    }
+}
