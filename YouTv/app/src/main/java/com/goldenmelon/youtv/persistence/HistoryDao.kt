a927525e735791ca77fe40660a68bb5fd548c244
makeman0405 <71001872+makeman0405@users.noreply.github.com>
2020-12-16 21:47:33 +0900

    Create HistoryDao.kt
    Room Dao Interface 생성

---
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/persistence/HistoryDao.kt
@@ -0,0 +1,21 @@
+package com.goldenmelon.youtv.persistence
+
+import androidx.room.*
+
+@Dao
+interface HistoryDao {
+    @Query("SELECT * FROM history ORDER BY date DESC")
+    fun getAll(): List<History>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(history: History)
+
+    @Update
+    fun update(history: History)
+
+    @Delete
+    fun delete(history: History)
+
+    @Query("DELETE FROM history WHERE video_id = :videoId")
+    fun deleteByVideoId(videoId: String)
+}
