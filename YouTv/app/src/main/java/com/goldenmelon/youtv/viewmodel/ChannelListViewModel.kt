77bc263ee7b015e0f9dc5963c5814c51a0579550
makeman0405 <71001872+makeman0405@users.noreply.github.com>
2020-12-16 16:07:27 +0900

    [Bug fix] Fatal Exception: java.util.ConcurrentModificationException
    코루틴 동기화 치리
    
    Fatal Exception: java.util.ConcurrentModificationException
           at java.util.ArrayList$Itr.next(ArrayList.java:860)
           at com.goldenmelon.youtv.ui.fragment.ContentListFragment$observeData$1.onChanged(ContentListFragment.kt:129)
           at com.goldenmelon.youtv.ui.fragment.ContentListFragment$observeData$1.onChanged(ContentListFragment.kt:26)
           at androidx.lifecycle.LiveData.considerNotify(LiveData.java:131)
           at androidx.lifecycle.LiveData.dispatchingValue(LiveData.java:149)
           at androidx.lifecycle.LiveData.setValue(LiveData.java:307)
           at androidx.lifecycle.MutableLiveData.setValue(MutableLiveData.java:50)
           at com.goldenmelon.youtv.viewmodel.MainListViewModel$loadContents$1.invokeSuspend(MainListViewModel.kt:27)
           at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(ContinuationImpl.kt:33)
           at kotlinx.coroutines.DispatchedTask.run(DispatchedTask.kt:56)
           at android.os.Handler.handleCallback(Handler.java:789)
           at android.os.Handler.dispatchMessage(Handler.java:98)
           at android.os.Looper.loop(Looper.java:164)
           at android.app.ActivityThread.main(ActivityThread.java:6944)
           at java.lang.reflect.Method.invoke(Method.java)
           at com.android.internal.os.Zygote$MethodAndArgsCaller.run(Zygote.java:327)
           at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1374)

--- YouTv/app/src/main/java/com/goldenmelon/youtv/viewmodel/ChannelListViewModel.kt
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/viewmodel/ChannelListViewModel.kt
@@ -1,6 +1,7 @@
 package com.goldenmelon.youtv.viewmodel
 
 import android.app.Application
+//import android.util.Log
 import android.webkit.CookieManager
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
@@ -11,13 +12,19 @@ import com.goldenmelon.youtv.utils.MAIN_URL
 import com.goldenmelon.youtv.utils.json.contentData
 import com.google.gson.GsonBuilder
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 class ChannelListViewModel(application: Application) : AndroidViewModel(application),
     ContentListViewModel {
+
+    val mutex = Mutex();
+    //var testCallCount: Int = 0;
+
     override val contents: MutableLiveData<MutableList<Content>> by lazy {
         MutableLiveData<MutableList<Content>>()
     }
@@ -25,10 +32,13 @@ class ChannelListViewModel(application: Application) : AndroidViewModel(applicat
     override fun loadContents(param: String?) {
         if (!param.isNullOrBlank()) {
             viewModelScope.launch {
-                val value = async(Dispatchers.IO) {
-                    crawling(param)
+                mutex.withLock {
+                    //Log.d(TAG, "1.loadContents: $param")
+                    //testCallCount++
+                    //Log.d(TAG, "2.loadContents: $param, $testCallCount")
+                    contents.value = crawling(param)
+                    //Log.d(TAG, "3.loadContents: $param, $testCallCount")
                 }
-                contents.value = value.await()
             }
         }
     }
@@ -39,87 +49,91 @@ class ChannelListViewModel(application: Application) : AndroidViewModel(applicat
     }
 
     private suspend fun crawling(channelWebpage: String): MutableList<Content> {
-        var list: MutableList<Content> = contents.value ?: mutableListOf<Content>()
+        return withContext(Dispatchers.IO) {
+            var list: MutableList<Content> = contents.value ?: mutableListOf<Content>()
 
-        //video filter
-        val connection = Jsoup.connect(CHANNEL_URL + channelWebpage + "/videos?view=0")
+            //video filter
+            val connection = Jsoup.connect(CHANNEL_URL + channelWebpage + "/videos?view=0")
 
-        CookieManager.getInstance().getCookie(MAIN_URL)?.let {
-            connection.cookie("Cookie", it)
-        }
+            CookieManager.getInstance().getCookie(MAIN_URL)?.let {
+                connection.cookie("Cookie", it)
+            }
 
-        val document: Document
-        try {
-            document = connection.get()
-        } catch (e: java.io.IOException) {
-            return list
-        }
+            val document: Document
+            try {
+                document = connection.get()
+            } catch (e: java.io.IOException) {
+                return@withContext list
+            }
 
-        val elements = document.getElementsByTag("script")
-        for (element in elements) {
-            if (element.html().contains("var ytInitialData = ")) {
-                var json = element.html().trim()
-                    .split("var ytInitialData = ")[1].split("};")[0] + "}"
+            val elements = document.getElementsByTag("script")
+            for (element in elements) {
+                if (element.html().contains("var ytInitialData = ")) {
+                    var json = element.html().trim()
+                        .split("var ytInitialData = ")[1].split("};")[0] + "}"
 
-                val gson = GsonBuilder().create()
-                val data =
-                    gson.fromJson(json, contentData::class.java)
+                    val gson = GsonBuilder().create()
+                    val data =
+                        gson.fromJson(json, contentData::class.java)
 
-                //fatal
-                data.contents?.twoColumnBrowseResultsRenderer?.let {
-                    val items =
-                        it.tabs[1].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].gridRenderer?.items
-                    items?.forEach { content ->
-                        content.gridVideoRenderer?.let {
-                            val tempContent = Content(it.videoId)
-                            if (!list.contains(tempContent)) {
-                                tempContent.thumbnail = it.thumbnail.thumbnails.last().url
+                    //fatal
+                    data.contents?.twoColumnBrowseResultsRenderer?.let {
+                        val items =
+                            it.tabs[1].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].gridRenderer?.items
+                        items?.forEach { content ->
+                            content.gridVideoRenderer?.let {
+                                val tempContent = Content(it.videoId)
+                                if (!list.contains(tempContent)) {
+                                    tempContent.thumbnail = it.thumbnail.thumbnails.last().url
 
-                                if (it.title.simpleText != null) {
-                                    tempContent.title = it.title.simpleText
-                                } else {
-                                    if (!it.title.runs.isNullOrEmpty()) {
-                                        tempContent.title = it.title.runs[0].text
-                                    }
-                                }
-
-                                //content.lengthText = "${it.lengthText?.simpleText}"
-
-                                tempContent.subTitle =
-                                    if (it.publishedTimeText?.simpleText != null) "${it.viewCountText?.simpleText} • ${it.publishedTimeText.simpleText}"
-                                    else "${
-                                        it.viewCountText?.runs?.get(
-                                            0
-                                        )?.text
-                                    }${
-                                        it.viewCountText?.runs?.let { array ->
-                                            if (array.size > 1) array[1].text else ""
-                                        }
-                                    }"
-
-
-                                if (!it.thumbnailOverlays.isNullOrEmpty()) {
-                                    for (thumbnailOverlay in it.thumbnailOverlays) {
-                                        if (thumbnailOverlay.thumbnailOverlayTimeStatusRenderer != null) {
-                                            tempContent.lengthText =
-                                                thumbnailOverlay.thumbnailOverlayTimeStatusRenderer.text.simpleText
-                                            break
+                                    if (it.title.simpleText != null) {
+                                        tempContent.title = it.title.simpleText
+                                    } else {
+                                        if (!it.title.runs.isNullOrEmpty()) {
+                                            tempContent.title = it.title.runs[0].text
                                         }
                                     }
-                                }
 
-                                list.add(tempContent)
+                                    //content.lengthText = "${it.lengthText?.simpleText}"
+
+                                    tempContent.subTitle =
+                                        if (it.publishedTimeText?.simpleText != null) "${it.viewCountText?.simpleText} • ${it.publishedTimeText.simpleText}"
+                                        else "${
+                                            it.viewCountText?.runs?.get(
+                                                0
+                                            )?.text
+                                        }${
+                                            it.viewCountText?.runs?.let { array ->
+                                                if (array.size > 1) array[1].text else ""
+                                            }
+                                        }"
+
+
+                                    if (!it.thumbnailOverlays.isNullOrEmpty()) {
+                                        for (thumbnailOverlay in it.thumbnailOverlays) {
+                                            if (thumbnailOverlay.thumbnailOverlayTimeStatusRenderer != null) {
+                                                tempContent.lengthText =
+                                                    thumbnailOverlay.thumbnailOverlayTimeStatusRenderer.text.simpleText
+                                                break
+                                            }
+                                        }
+                                    }
+
+                                    list.add(tempContent)
+                                }
                             }
                         }
                     }
                 }
+
             }
 
+            //result
+            list
         }
-        return list
     }
 
     companion object {
-        const val TAG = "SearchContentViewModel"
+        const val TAG = "ChannelListViewModel"
     }
 }
--- YouTv/app/src/main/java/com/goldenmelon/youtv/viewmodel/SearchListViewModel.kt
+++ YouTv/app/src/main/java/com/goldenmelon/youtv/viewmodel/SearchListViewModel.kt
@@ -11,13 +11,19 @@ import com.goldenmelon.youtv.utils.SEARCH_URL
 import com.goldenmelon.youtv.utils.json.searchContentData
 import com.google.gson.GsonBuilder
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 class SearchListViewModel(application: Application) : AndroidViewModel(application),
     ContentListViewModel {
+
+    val mutex = Mutex();
+    //var testCallCount: Int = 0;
+
     override val contents: MutableLiveData<MutableList<Content>> by lazy {
         MutableLiveData<MutableList<Content>>()
     }
@@ -25,10 +31,14 @@ class SearchListViewModel(application: Application) : AndroidViewModel(applicati
     override fun loadContents(param: String?) {
         if (!param.isNullOrBlank()) {
             viewModelScope.launch {
-                val value = async(Dispatchers.IO) {
-                    crawling(param)
+                mutex.withLock {
+                    //Log.d(TAG, "1.loadContents: $param")
+                    //testCallCount++
+                    //Log.d(TAG, "2.loadContents: $param, $testCallCount"
+                    contents.value = crawling(param)
+                    //Log.d(TAG, "3.loadContents: $param, $testCallCount")
+
                 }
-                contents.value = value.await()
             }
         }
     }
@@ -39,75 +49,78 @@ class SearchListViewModel(application: Application) : AndroidViewModel(applicati
     }
 
     private suspend fun crawling(keyword: String): MutableList<Content> {
-        var list: MutableList<Content> = contents.value ?: mutableListOf<Content>()
+        return withContext(Dispatchers.IO) {
+            var list: MutableList<Content> = contents.value ?: mutableListOf<Content>()
 
-        //video filter
-        val connection = Jsoup.connect(SEARCH_URL + keyword + "&sp=EgIQAQ%253D%253D")
-        CookieManager.getInstance().getCookie(MAIN_URL)?.let {
-            connection.cookie("Cookie", it)
-        }
+            //video filter
+            val connection = Jsoup.connect(SEARCH_URL + keyword + "&sp=EgIQAQ%253D%253D")
+            CookieManager.getInstance().getCookie(MAIN_URL)?.let {
+                connection.cookie("Cookie", it)
+            }
 
-        val document: Document
-        try {
-            document = connection.get()
-        } catch (e: java.io.IOException) {
-            return list
-        }
+            val document: Document
+            try {
+                document = connection.get()
+            } catch (e: java.io.IOException) {
+                return@withContext list
+            }
 
-        val elements = document.getElementsByTag("script")
+            val elements = document.getElementsByTag("script")
 
-        for (element in elements) {
-            if (element.html().contains("var ytInitialData = ")) {
-                val json = element.html().trim()
-                    .split("var ytInitialData = ")[1].split("};")[0] + "}"
-                val gson = GsonBuilder().create()
-                val data =
-                    gson.fromJson(json, searchContentData::class.java)
+            for (element in elements) {
+                if (element.html().contains("var ytInitialData = ")) {
+                    val json = element.html().trim()
+                        .split("var ytInitialData = ")[1].split("};")[0] + "}"
+                    val gson = GsonBuilder().create()
+                    val data =
+                        gson.fromJson(json, searchContentData::class.java)
 
-                data.contents.twoColumnSearchResultsRenderer?.let {
-                    for (content in it.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents) {
-                        content.videoRenderer?.let {
-                            var content = Content(it.videoId)
-                            if (!list.contains(content)) {
-                                content.thumbnail = it.thumbnail.thumbnails[0].url
+                    data.contents.twoColumnSearchResultsRenderer?.let {
+                        for (content in it.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents) {
+                            content.videoRenderer?.let {
+                                var content = Content(it.videoId)
+                                if (!list.contains(content)) {
+                                    content.thumbnail = it.thumbnail.thumbnails[0].url
 
-                                if (it.title.simpleText != null) {
-                                    content.title = it.title.simpleText
-                                } else {
-                                    if (!it.title.runs.isNullOrEmpty()) {
-                                        content.title = it.title.runs[0].text
-                                    }
-                                }
-
-                                content.lengthText = it.lengthText?.simpleText?:"null"
-                                content.ownerText = it.ownerText.runs[0].text
-                                content.subTitle =
-                                    if (it.publishedTimeText?.simpleText != null) "${it.ownerText.runs[0].text} • ${it.viewCountText?.simpleText} • ${it.publishedTimeText?.simpleText}"
-                                    else "${it.ownerText.runs[0].text} • ${
-                                        it.viewCountText?.runs?.get(
-                                            0
-                                        )?.text
-                                    }${
-                                        it.viewCountText?.runs?.let { array ->
-                                            if (array.size > 1) array[1].text else ""
+                                    if (it.title.simpleText != null) {
+                                        content.title = it.title.simpleText
+                                    } else {
+                                        if (!it.title.runs.isNullOrEmpty()) {
+                                            content.title = it.title.runs[0].text
                                         }
-                                    }"
+                                    }
 
-                                content.channelThumbnail =
-                                    it.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url
+                                    content.lengthText = it.lengthText?.simpleText ?: "null"
+                                    content.ownerText = it.ownerText.runs[0].text
+                                    content.subTitle =
+                                        if (it.publishedTimeText?.simpleText != null) "${it.ownerText.runs[0].text} • ${it.viewCountText?.simpleText} • ${it.publishedTimeText?.simpleText}"
+                                        else "${it.ownerText.runs[0].text} • ${
+                                            it.viewCountText?.runs?.get(
+                                                0
+                                            )?.text
+                                        }${
+                                            it.viewCountText?.runs?.let { array ->
+                                                if (array.size > 1) array[1].text else ""
+                                            }
+                                        }"
 
-                                content.channelWebpage =
-                                    it.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.navigationEndpoint.commandMetadata.webCommandMetadata.url
+                                    content.channelThumbnail =
+                                        it.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url
 
-                                list.add(content)
+                                    content.channelWebpage =
+                                        it.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.navigationEndpoint.commandMetadata.webCommandMetadata.url
+
+                                    list.add(content)
+                                }
                             }
                         }
                     }
                 }
             }
-        }
 
-        return list
+            //result
+            list
+        }
     }
 
     companion object {
